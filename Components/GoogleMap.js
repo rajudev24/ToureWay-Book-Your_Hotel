f6b1756a74cec90a60eda0e565e0ee2362c02834
rajudev24 <rajudevit94@gmail.com>
2022-01-11 21:20:06 +0600

    added few card

--- Components/GoogleMap.js
+++ Components/GoogleMap.js
@@ -8,12 +8,11 @@ const GoogleMap = () => {
         latitude: 37.7577,
         longitude: -122.4376,
         zoom: 11,
-    })
+    });
     return (
         <ReactMapGL
-            mapStyle="mapbox://styles/rajudeb/ckya467fz1l6p14qsmepgwxko"
+            mapStyle='mapbox://styles/rajudeb/ckya467fz1l6p14qsmepgwxko'
             mapboxApiAccessToken={process.env.mapbox_key}
-
             {...viewPort}
         >
 
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Header from '../Components/Header'
 import ImageCard from '../Components/ImageCard'
 import Place from '../Components/place'
 
-export default function Home({places, images}) {
+export default function Home({ places, images }) {
 
   return (
     <div className=''>
@@ -17,47 +17,80 @@ export default function Home({places, images}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <Header/>
-      <Banner/>
+      <Header />
+      <Banner />
 
       <main className='max-w-5xl mx-auto px-8 sm:px-17 '>
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold'>Explore Nearby</h2>
-         
+
           <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-          {places?.map(place => <Place 
-          key={place._id}
-          place = {place}
-          /> )}
+            {places?.map(place => <Place
+              key={place._id}
+              place={place}
+            />)}
           </div>
         </section>
         <section>
           <h2 className='text-4xl font-semibold py-8'>Explore Places & Live Anywhere</h2>
           <div className='flex space-x-3 overflow-scroll scrollbar-hide  p-3 -ml-3'>
             {
-              images?.map(image => <ImageCard 
+              images?.map(image => <ImageCard
                 key={image._id}
-                image = {image}
-              /> )
+                image={image}
+              />)
             }
           </div>
         </section>
-        <FooterBanner/>
+        <section className=' md:flex md:space-x-3 ' >
+          <div className='relative py-10 cursor-pointer' >
+            <div className='relative h-[400px] min-w-[400px]  md:h-[450px] md:min-w-[480px]'>
+              <Image src='/img/pic-1.jpg'
+                layout='fill'
+                objectFit='cover'
+                alt='img'
+                className='rounded-2xl'
+              />
+            </div>
+            <div className='absolute top-[90px] left-8'>
+              <h2 className='text-2xl md:text-4xl mb-3 w-50 text-white'>Six Things Solo Travel Teaches You</h2>
+              <p>You might stumble across a place or activity that</p>
+              <button className='text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-4 hover:opacity-80'>Get Inspired</button>
+            </div>
+          </div>
+          <div className='relative py-10 cursor-pointer' >
+            <div className='relative h-[400px] min-w-[400px]  md:h-[450px] md:min-w-[480px]'>
+              <Image src='/img/pic-2.jpg'
+                layout='fill'
+                objectFit='cover'
+                alt='img'
+                className='rounded-2xl'
+              />
+            </div>
+            <div className='absolute top-[90px] left-8'>
+              <h2 className='text-2xl md:text-4xl mb-3 w-50 text-white'> Mountain Travel Destinations in the World</h2>
+              <p>Learn which mountain destinations</p>
+              <button className='text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-4 hover:opacity-80'>Get Inspired</button>
+            </div>
+          </div>
+          
+        </section>
+        <FooterBanner />
       </main>
       <footer>
-        <Footer/>
+        <Footer />
       </footer>
     </div>
   )
 }
 
-export async function getStaticProps(){
-  const places = await fetch('https://whispering-tundra-09529.herokuapp.com/places').then(res =>res.json());
+export async function getStaticProps() {
+  const places = await fetch('https://whispering-tundra-09529.herokuapp.com/places').then(res => res.json());
 
-  const images = await fetch('https://whispering-tundra-09529.herokuapp.com/images').then(res=> res.json())
+  const images = await fetch('https://whispering-tundra-09529.herokuapp.com/images').then(res => res.json())
 
   return {
-    props:{
+    props: {
       places,
       images
     }
